1b9915cfa451f3fc4714fca58c666c735da63b45
KE0BQA <udyllan@protonmail.ch>
2017-08-29 14:30:04 -0500

    Initial Start couts complete

---
+++ Week_2/program1.cpp
@@ -0,0 +1,19 @@
+/* 	program1.cpp
+	Dyllan Uplinger
+	20170829
+	J633V226
+	Homework #01
+	Prompt user for two Numbers and choice of operation; will display results
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Enter First Integer \n";
+	cout <<"Enter Second Integar \n";
+	cout <<"Available Operations are:\n 1.) Addition\n 2.) Subtraction\n 3.) Multiplication\n 4.) Division";
+	return 0;
+}
