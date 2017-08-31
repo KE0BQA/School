88d289939efb32e5324197f8163e964dbb7cda0c
KE0BQA <udyllan@protonmail.ch>
2017-08-31 13:55:30 -0500

    Lab Start 20170831

--- Week_2/program1.cpp
+++ Week_2/program1.cpp
@@ -1,4 +1,5 @@
-/* 	program1.cpp
+/* 	
+	program1.cpp
 	Dyllan Uplinger
 	20170831
 	J633V226
@@ -26,34 +27,41 @@ int main()
 	cout <<"Available Operations are:\n\t1.) Addition\n\t2.) Subtraction\n\t3.) Multiplication\n\t4.) Division\nPlease Select a Number:\n";
 	cin >> operation;
 
+	//Preventing Divide by 0
 	if (operation == 4 && secondIntegar == 0)
 		{
 			cout <<"Can Not Divide by Zero; Program Exiting\n";
 			return 0;
 		}
+
+	//Preventing Invalid Operations
+	if (operation > 4 || operation < 1)
+		{
+		cout <<" \n Invalid Operation. Valid Choices were 1, 2, 3 or 4. Quitting Program\n";
+		}
+
 	if (operation == 1)
 		{
 			finalValue = firstIntegar + secondIntegar;
 			cout << firstIntegar << " + " << secondIntegar << " = " << finalValue <<"\n";
 		}
+
 	if (operation == 2)
 		{
 			finalValue = firstIntegar - secondIntegar;
 			cout << firstIntegar << " - " << secondIntegar << " = " << finalValue <<"\n";
 		}
+
 	if (operation == 3)
 		{
 			finalValue = firstIntegar * secondIntegar;
 			cout << firstIntegar << " * " << secondIntegar << " = " << finalValue <<"\n";
 		}
+
 	if (operation == 4)
 		{
 			finalValue = firstIntegar / secondIntegar;
 			cout << firstIntegar << " / " << secondIntegar << " = " << finalValue <<"\n";
-		}
-	if (operation > 4 || operation < 1)
-		{
-		cout <<" \n Invalid Operation. Valid Choices were 1, 2, 3 or 4. Quitting Program\n";
 		}
 
 	return 0;
