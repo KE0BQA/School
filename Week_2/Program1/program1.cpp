14157ef7c8c7c9d13a64af97503f9a5e35be8444
KE0BQA <udyllan@protonmail.ch>
2017-08-31 14:09:43 -0500

    Further Syntax Changes

--- Week_2/Program1/program1.cpp
+++ Week_2/Program1/program1.cpp
@@ -1,6 +1,7 @@
-/* 	program1.cpp
+/* 	
+	program1.cpp
 	Dyllan Uplinger
-	20170829
+	20170831
 	J633V226
 	Homework #01
 	Prompt user for two Numbers and choice of operation; will display results
@@ -26,34 +27,31 @@ int main()
 	cout <<"Available Operations are:\n\t1.) Addition\n\t2.) Subtraction\n\t3.) Multiplication\n\t4.) Division\nPlease Select a Number:\n";
 	cin >> operation;
 
+	//Preventing Divide by 0
 	if (operation == 4 && secondIntegar == 0)
 		{
 			cout <<"Can Not Divide by Zero; Program Exiting\n";
 			return 0;
 		}
-	if (operation == 1)
-		{
-			finalValue = firstIntegar + secondIntegar;
-			cout << firstIntegar << " + " << secondIntegar << " = " << finalValue <<"\n";
-		}
-	if (operation == 2)
-		{
-			finalValue = firstIntegar - secondIntegar;
-			cout << firstIntegar << " - " << secondIntegar << " = " << finalValue <<"\n";
-		}
-	if (operation == 3)
-		{
-			finalValue = firstIntegar * secondIntegar;
-			cout << firstIntegar << " * " << secondIntegar << " = " << finalValue <<"\n";
-		}
-	if (operation == 4)
-		{
-			finalValue = firstIntegar / secondIntegar;
-			cout << firstIntegar << " / " << secondIntegar << " = " << finalValue <<"\n";
-		}
+
+	//Preventing Invalid Operations
 	if (operation > 4 || operation < 1)
 		{
 		cout <<" \n Invalid Operation. Valid Choices were 1, 2, 3 or 4. Quitting Program\n";
+		}
+
+	if (operation == 1)	{
+			finalValue = firstIntegar + secondIntegar;
+			cout << firstIntegar << " + " << secondIntegar << " = " << finalValue <<"\n";
+		} else if (operation == 2) {
+			finalValue = firstIntegar - secondIntegar;
+			cout << firstIntegar << " - " << secondIntegar << " = " << finalValue <<"\n";
+		} else if (operation == 3) {
+			finalValue = firstIntegar * secondIntegar;
+			cout << firstIntegar << " * " << secondIntegar << " = " << finalValue <<"\n";
+		} else if (operation == 4) {
+			finalValue = firstIntegar / secondIntegar;
+			cout << firstIntegar << " / " << secondIntegar << " = " << finalValue <<"\n";
 		}
 
 	return 0;
