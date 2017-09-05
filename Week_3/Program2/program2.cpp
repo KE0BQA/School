a62c89d03144dac30832ddd0d8c2a91b9283c321
KE0BQA <udyllan@protonmail.ch>
2017-09-05 13:41:11 -0500

    Class End 20170905

--- Week_3/Program2/program2.cpp
+++ Week_3/Program2/program2.cpp
@@ -1,11 +1,46 @@
 /*
-	
+	program2.cpp
+	Dyllan Uplinger
+	20170905
+	J633V226
+	Homework #02
+	Draw to the terminal 3 shapes
 */
 
 #include <cmath>
+#include <iostream>
 
-int main(void)
+int main()
 {
+	std::cout <<"\n";
+
+	//Triangle
+	for (int i = 8; i > 0; i--){
+		for (int j = i; j > 0; (j--)){
+			std::cout <<"#";
+			std::cout <<" ";
+		}
+	std::cout <<"\n";
+	}
+
+	//Triangle
+
+	std::cout <<"\n";
+
+	//Diamond
+	std::cout <<"#";
+	std::cout <<" ";
+	std::cout <<"\n";
+	//Diamond
+
+	std::cout <<"\n";
+
+	//X-Shape
+	std::cout <<"#";
+	std::cout <<" ";
+	std::cout <<"\n";
+	//X-Shape
+
 	return 0;
 }
 
