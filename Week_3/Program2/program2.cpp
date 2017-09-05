4b38e7c38220c5a73e0dece7fab46aa2d6b77d6d
KE0BQA <udyllan@protonmail.ch>
2017-09-05 14:46:56 -0500

    NWAI Prints to Terminal w/o Loop

--- Week_3/Program2/program2.cpp
+++ Week_3/Program2/program2.cpp
@@ -15,31 +15,54 @@ int main()
 	std::cout <<"\n";
 
 	//Triangle
-	for (int i = 8; i > 0; i--){
-		for (int j = i; j > 0; (j--)){
-			std::cout <<"#";
-			std::cout <<" ";
-		}
-	std::cout <<"\n";
-	}
+	int a=0, b=0, NUM=8;
 
+	for (a=-NUM; a<=NUM; a++) {
+		for (b=-NUM; b<=NUM; b++) {
+			if ( abs (a)+abs (b)<=NUM) {
+				std::cout <<"#";
+			} else {
+				std::cout <<" ";
+			}
+		}
+	}
+	std::cout <<"\n";
 	//Triangle
 
 	std::cout <<"\n";
 
 	//Diamond
-	std::cout <<"#";
-	std::cout <<" ";
+	int c=0, d=0, NUM1=8;
+
+	for (c=-NUM1; c<=NUM1; c++) {
+		for (d=-NUM; d<=NUM1; d++) {
+			if ( abs (c) + abs (d) <=NUM1) {
+				std::cout <<"#";
+			} else {
+				std::cout <<" ";
+			}
+		}
+	}
 	std::cout <<"\n";
 	//Diamond
 
 	std::cout <<"\n";
 
 	//X-Shape
-	std::cout <<"#";
-	std::cout <<" ";
+	int e=0, f=0, NUM2=4;
+
+	for (e=-NUM2; e<=NUM2; e++) {
+		for (f=-NUM2; f<=NUM2; f++) {
+			if( (abs (e) == abs (f))<=NUM2){
+				std::cout <<"#";
+			} else {
+				std::cout <<" ";
+			}
+		}
+	}
 	std::cout <<"\n";
 	//X-Shape
+
 
 	return 0;
 }
