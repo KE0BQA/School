ba5c7d684186fbbd16a44064241cdf07d957ab3a
KE0BQA <udyllan@protonmail.ch>
2017-09-12 15:07:05 -0500

    Lab End 20170912

--- Week_4/Program3/program3.cpp
+++ Week_4/Program3/program3.cpp
@@ -9,37 +9,60 @@
 
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-double "name"();
-double "name"();
-double "name"();
-
+/*
+double calulateMonthlyInterest(double loanAmout, double interest)
+double calculateMonthlyPrinciple(double principle, double interest)
+*/
 
 int main(void)
 {
-	cout<<"\n";
-	cin >> ;
+	double loanAmount;
+	double interest;
+	double payment;
 
-	cout<<"\n";
-	cin >> ;
+
+	cout<<"Amount of Loan\n";
+	cin >> loanAmount;
+
+	cout<<"Interest Rate\n";
+	cin >> interest;
+	
+	cout<<"Monthly Payment\n";
+	cin >> payment;
+
+	for (int year = 1; year <= 3; year++) {
+			cout << "Year " << year << setw(20) << "Interest" << setw(25) << "Principal";
+			cout <<endl;
+			cout << setw(10) << "---" << setw(16) <<  "--------" << setw(25) << "---------";
+			cout<<endl;
+		for (int month = 1; month <= 12; month++) {
+			cout << setw(10)  << month << setw(16) << "." << setw(25) << ".";
+			cout <<endl;
+		}
+			cout << setw(10) << "---" << setw(16) <<  "--------" << setw(25) << "---------" <<endl;
+			cout << endl;
+			cout << "Year " << year << " Totals: " << setw(16) << "yearlyInterestTotal" << setw(25) << "yearlyPrincipalTotal" <<endl;
+			cout << "Remaining Balance at year end:" << setw(21) << "yearlyBalanceTotal" << endl;
+	}
 
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(2);
 
-	cout << 
+	cout <<"EndP"; 
 
 	return 0;
 }
 
-double {
+/* double calulateMonthlyInterest(loanAmout, interest) {
 
 }
-double {
+
+double calculateMonthlyPrinciple(principle, interest) {
 
 }
-double {
-
-}
+*/
