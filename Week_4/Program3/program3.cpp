1bfd88809080c14833280bad6d7a140af8a3d18f
KE0BQA <udyllan@protonmail.ch>
2017-09-12 13:22:25 -0500

    Class End 20170912 Homework3 Start

---
+++ Week_4/Program3/program3.cpp
@@ -0,0 +1,45 @@
+
+/*
+	program3.cpp
+	Dyllan Uplinger
+	J633V226
+	Homework #03
+	Calculate Loan Information
+*/
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double "name"();
+double "name"();
+double "name"();
+
+
+int main(void)
+{
+	cout<<"\n";
+	cin >> ;
+
+	cout<<"\n";
+	cin >> ;
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+
+	cout << 
+
+	return 0;
+}
+
+double {
+
+}
+double {
+
+}
+double {
+
+}
