dc2967046874463789b18b135531805fe977d52d
KE0BQA <udyllan@protonmail.ch>
2017-09-16 12:45:50 -0500

    update

--- Week_4/Program3/program3func.cpp
+++ Week_4/Program3/program3func.cpp
@@ -15,11 +15,11 @@
   5. Repeat
   6. Calculate Total Yearly Interest
   7. Calculate Total Monthly Interest
-  8. Calculate Total Yearly Balance Remaining 
+  8. Calculate Total Yearly Balance Remaining
 */
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 
@@ -54,31 +54,27 @@ double printToScreen(double loanAmount_2, double interest_2, double payment_2) {
   for (int year = 1; year <= 3; year++) {
     double yearlyPrincipal = 0;
     double yearlyInterest = 0;
-    cout << "Year " << year;
-    cout << setw(20) << "Interest";
-    cout << setw(25) << "Principal";
+    cout << "Year " << year << setw(20) << "Interest" << setw(25)
+         << "Principal";
     cout << endl;
-    cout << setw(10) << "---";
-    cout << setw(16) << "--------";
-    cout << setw(25) << "---------";
+    cout << setw(10) << "---" << setw(16) << "--------" << setw(25)
+         << "---------";
     cout << endl;
     for (int month = 1; month <= 12; month++) {
-      cout << setw(10) << month;
-      cout << setw(16) << calculateMonthlyInterest(loanAmount_2, interest_2);
-      cout << setw(25) << (payment_2 - calculateMonthlyInterest(loanAmount_2, interest_2));
-      yearlyPrincipal += (payment_2 - calculateMonthlyInterest(loanAmount_2, interest_2));
+      cout << setw(10) << month << setw(16)
+           << calculateMonthlyInterest(loanAmount_2, interest_2) << setw(25)
+           << (payment_2 - calculateMonthlyInterest(loanAmount_2, interest_2));
+      yearlyPrincipal +=
+          (payment_2 - calculateMonthlyInterest(loanAmount_2, interest_2));
       yearlyInterest += (calculateMonthlyInterest(loanAmount_2, interest_2));
       loanAmount_2 += calculateMonthlyInterest(loanAmount_2, interest_2);
       loanAmount_2 -= payment_2;
       cout << endl;
     }
-    cout << setw(10) << "---";
-    cout << setw(16) << "--------";
-    cout << setw(25) << "---------";
-    cout << endl;
-    cout << "Year " << year << " Totals: ";
-    cout << setw(11) << yearlyInterest;
-    cout << setw(25) << yearlyPrincipal;
+    cout << setw(10) << "---" << setw(16) << "--------" << setw(25)
+         << "---------" << endl;
+    cout << "Year " << year << " Totals: " << setw(11) << yearlyInterest
+         << setw(25) << yearlyPrincipal;
     cout << endl;
     cout << "Remaining Balance at year end:";
     cout << setw(21) << loanAmount_2;
@@ -90,6 +86,6 @@ double printToScreen(double loanAmount_2, double interest_2, double payment_2) {
 
 double calculateMonthlyInterest(double loanAmount_3, double interest_3) {
   double monthlyInterest;
-  monthlyInterest = (loanAmount_3 * (interest_3/12));
+  monthlyInterest = (loanAmount_3 * (interest_3 / 12));
   return monthlyInterest;
 }
