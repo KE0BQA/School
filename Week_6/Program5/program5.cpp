1f4767ed85f2f84277e9f737ddf5d5d3116908aa
KE0BQA <udyllan@protonmail.ch>
2017-10-05 15:27:48 -0500

    Functions removed 	Not Needed any more due to everything being posisble through the 	main function
    Program Completed

--- Week_6/Program5/program5.cpp
+++ Week_6/Program5/program5.cpp
@@ -16,15 +16,31 @@
 
 using std::cout;
 
-void writeBlock(void);
-
 const int SIZE = 5;
 const char dog[SIZE] = {'B', 'I', 'N', 'G', 'O'};
 
-int main() { writeBlock(); }
-
-void writeBlock(void) {
-  cout << "There was a farmer had a dog" << std::endl
-       << "and" << dog[SIZE] << "was his name-o" << dog[SIZE] << dog[SIZE]
-       << dog[SIZE] << "and" << dog[SIZE] << "was his name-o";
+int main() {
+  for (int num = 0; num < 6; num++) {
+    cout << "There was a farmer had a dog" << std::endl << "and ";
+    for (int fullName = 0; fullName < 5; fullName++) {
+      cout << dog[fullName] << " ";
+    }
+    cout << " was his name-o" << std::endl;
+    for (int row = 0; row < 3; row++) {
+      for (int line = 0; line < 5; line++) {
+        if (line <= (num - 1)) {
+          cout << "* ";
+        } else {
+          cout << dog[line] << " ";
+        }
+      }
+      cout << std::endl;
+    }
+    cout << "and ";
+    for (int fullName = 0; fullName < 5; fullName++) {
+      cout << dog[fullName] << " ";
+    }
+    cout << " was his name-o" << std::endl << std::endl;
+  }
+  return 0;
 }
