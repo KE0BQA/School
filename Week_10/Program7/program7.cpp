0e1249dfc9f9077c217271e0b994382d578926ea
KE0BQA <udyllan@protonmail.ch>
2017-10-26 13:17:39 -0500

    Initial Prep Work for Program7

---
+++ Week_10/Program7/program7.cpp
@@ -0,0 +1,13 @@
+/*
+  program7.cpp
+  Dyllan Uplinger
+  J633V226
+  Homework #07
+
+  Copyright 2017
+  */
+
+/*
+  Google C++ Style Guide
+  https://google.github.io/styleguide/cppguide.html
+  */
