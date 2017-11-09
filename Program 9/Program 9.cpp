07d43e358e8efe73793f8846887d1cded3960c1b
Dyllan <udyllan@protonmail.ch>
2017-11-09 16:51:30 -0600

    Added showWarmer Funch (Not Working as Intended)

--- Program 9/Program 9.cpp
+++ Program 9/Program 9.cpp
@@ -13,15 +13,20 @@ Copyright 2017
 #include <iomanip>
 #include <iostream>
 
+using std::cin;
 using std::cout;
 using std::ifstream;
 
 void input(double celsius[][24]);
-void print(double celsius[][24]);
+// void print(double celsius[][24]);
 void convert(double celsius[][24]);
+void showWarmer(double celsius[][24], double cutoff);
+double findAverage(double celsius[][24]);
+void findDailyMMM(double dayTemps[], double &max, double &min, double &mean);
 
 int main() {
   double celsius[7][24];
+  double cutoff = 0;
 
   cout.setf(std::ios::fixed);
   cout.setf(std::ios::showpoint);
@@ -29,6 +34,9 @@ int main() {
 
   input(celsius);
   convert(celsius);
+  cout << "Enter the value for which to find warmer temperatures (C): ";
+  cin >> cutoff;
+  showWarmer(celsius, cutoff);
   return 0;
 }
 
@@ -55,7 +63,7 @@ void convert(double celsius[][24]) {
     }
   }
 }
-void print(double celsius[][24]) {
+/* void print(double celsius[][24]) {
   for (int i = 0; i <= 23; i++) {
     for (int j = 0; j <= 6; j++) {
       cout << celsius[j][i] << " ";
@@ -63,3 +71,18 @@ void print(double celsius[][24]) {
     cout << std::endl;
   }
 }
+*/
+void showWarmer(double celsius[][24], double cutoff) {
+  for (int i = 0; i < 23; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (celsius[j][i] > cutoff)
+        cout << "\n"
+             << "At day " << j + 1 << ","
+             << " hour " << i + 1 << ","
+             << "the temp was " << celsius[j][i] << " C"
+             << "\n";
+    }
+  }
+}
+// double findAverage(double celsius[][24], int days) {}
+// void findDailyMMM(double dayTemps[], double &max, double &min, double &mean) {}
