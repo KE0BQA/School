b21d03efa325b48af02662aa6b2aae9c79bb2ef8
Dyllan <udyllan@protonmail.ch>
2017-11-14 15:01:00 -0600

    Lab End 20171114

--- Program 9/Program 9.cpp
+++ Program 9/Program 9.cpp
@@ -18,15 +18,15 @@ using std::cout;
 using std::ifstream;
 
 void input(double celsius[][24]);
-// void print(double celsius[][24]);
 void convert(double celsius[][24]);
-void showWarmer(double celsius[][24], double cutoff);
-double findAverage(double celsius[][24]);
-void findDailyMMM(double dayTemps[], double &max, double &min, double &mean);
+void showWarmer(double celsius[][24], int days);
+double findAverage(double celsius[][24], int days);
+void findDailyMaxMinMean(double dayTemps[], double &max, double &min,
+                         double &mean);
 
 int main() {
   double celsius[7][24];
-  double cutoff = 0;
+  int days = 7;
 
   cout.setf(std::ios::fixed);
   cout.setf(std::ios::showpoint);
@@ -34,9 +34,11 @@ int main() {
 
   input(celsius);
   convert(celsius);
-  cout << "Enter the value for which to find warmer temperatures (C): ";
-  cin >> cutoff;
-  showWarmer(celsius, cutoff);
+  showWarmer(celsius, days);
+  cout << "The average temperature over all the days was: "
+       << findAverage(celsius, days);
+
+
   return 0;
 }
 
@@ -48,41 +50,44 @@ void input(double celsius[][24]) {
     exit(1);
   }
   while (!(fin.eof())) {
-    for (int i = 0; i <= 23; i++) {
-      for (int j = 0; j <= 6; j++) {
-        fin >> celsius[j][i];
+    for (int day = 1; day <= 7; day++) {
+      for (int hour = 1; hour <= 24; hour++) {
+        fin >> celsius[day][hour];
       }
     }
     fin.close();
   }
 }
 void convert(double celsius[][24]) {
-  for (int i = 0; i <= 23; i++) {
-    for (int j = 0; j <= 6; j++) {
-      celsius[j][i] = ((celsius[j][i] - 32) * 5 / 9);
+  for (int day = 1; day <= 7; day++) {
+    for (int hour = 1; hour <= 24; hour++) {
+      celsius[day][hour] = ((celsius[day][hour] - 32) * 5 / 9);
     }
   }
 }
-/* void print(double celsius[][24]) {
-  for (int i = 0; i <= 23; i++) {
-    for (int j = 0; j <= 6; j++) {
-      cout << celsius[j][i] << " ";
-    }
-    cout << std::endl;
-  }
-}
-*/
-void showWarmer(double celsius[][24], double cutoff) {
-  for (int i = 0; i < 23; i++) {
-    for (int j = 0; j < 6; j++) {
-      if (celsius[j][i] > cutoff)
-        cout << "\n"
-             << "At day " << j + 1 << ","
-             << " hour " << i + 1 << ","
-             << "the temp was " << celsius[j][i] << " C"
-             << "\n";
+void showWarmer(double celsius[][24], int days) {
+  double cutoff = 0;
+  cout << "Enter the value for which to find warmer temperatures (C): ";
+  cin >> cutoff;
+  for (int day = 1; day <= days; day++) {
+    for (int hour = 1; hour <= 24; hour++) {
+      if (celsius[day][hour] > cutoff)
+        cout << "At day " << day << ","
+             << " hour " << hour << ","
+             << " the temp was " << celsius[day][hour] << " C" << std::endl;
     }
   }
 }
-// double findAverage(double celsius[][24], int days) {}
-// void findDailyMMM(double dayTemps[], double &max, double &min, double &mean) {}
+double findAverage(double celsius[][24], int days) {
+  double total = 0;
+  for (int day = 1; day <= days; day++) {
+    for (int hour = 1; hour <= 24; hour++) {
+      total += celsius[day][hour];
+    }
+  }
+  return (total / (days * 24));
+}
+void findDailyMaxMinMean(double dayTemps[], double &max, double &min,
+                         double &mean) {
+
+}
