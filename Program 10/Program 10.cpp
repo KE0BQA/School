dab7e18a9e7290d484f34c81f0531449a83a3ff4
Dyllan <udyllan@protonmail.ch>
2017-11-30 13:02:42 -0600

    Setting Up Conversion

--- Program 10/Program 10.cpp
+++ Program 10/Program 10.cpp
@@ -67,57 +67,80 @@ Zipcode fillZipcode(const string zip) {
     break;
   }
 }
-string romanToPOSTNET(const int r)
-{
+string romanToPOSTNET(const int r) {
+  // Loop through Zip
+  // Check Each Number
+  // Convert to Postnet
+  string temp;
   for (a[i]) {
     if (i == 0) {
-      temp += "11000"
-    }
-    else if (i == 1) {
-      temp += "00001"
+      temp += 11000;
+    } else if (i == 1) {
+      temp += 00011;
+    } else if (i == 2) {
+      temp += 00101
+    } else if (i == 3) {
+      temp += 00110;
+    } else if (i == 4) {
+      temp += 01001;
+    } else if (i == 5) {
+      temp += 01010;
+    } else if (i == 6) {
+      temp += 01100;
+    } else if (i == 7) {
+      temp += 10001;
+    } else if (i == 8) {
+      temp += 10010;
+    } else if (i == 9) {
+      temp += 10100;
     }
   }
   return string();
 }
 int postnetToRoman(const string p) {
-  int temp;
-  for (int i = 0; i < 26; i++) {
-    switch (p(i)) {
-    case 1:
-      switch (i % 5) {
-      case 1:
-        temp += 7;
-        break;
-      case 2:
-        temp += 4;
-        break;
-      case 3:
-        temp += 2;
-        break;
-      case 4:
-        temp += 1;
-        break;
-      }
-    }
+  // Break into groups of 5 Without bookends
+  // Reverse of above
+  if (i == 11000) {
+    temp += 0;
+  } else if (i == 00011) {
+    temp += 1;
+  } else if (i == 00101) {
+    temp += 2;
+  } else if (i == 00110) {
+    temp += 3;
+  } else if (i == 01001) {
+    temp += 4;
+  } else if (i == 01010) {
+    temp += 5;
+  } else if (i == 01100) {
+    temp += 6;
+  } else if (i == 10001) {
+    temp += 7;
+  } else if (i == 10010) {
+    temp += 8;
+  } else if (i == 10100) {
+    temp += 9;
   }
 }
 void printRomanZip(const Zipcode zip) {
   string RomanZip;
-
+  // print Zip Code
 }
 void printPOSTNET(const Zipcode zip) {
   string POSTNET;
+  // print postnet 1 = long 0 = short
 }
 void writeToFile(const Zipcode zip) {
-
+  // Save file with name of Zip Code
 }
 void processZip(int prompt) {
   Zipcode structure;
   string input;
-  if (prompt == 1)
+  if (prompt == 1) {
     cout << "Enter Zip:";
-  else
+  } else {
     cout << "Enter Postnet:";
+  }
   cin >> input;
   structure = fillZipcode(input);
 }
